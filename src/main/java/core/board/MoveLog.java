51192168ffa493c0e83fd8e3a4b464026416de44
Angelo Rigoni <angelo.rigoni05@gmail.coom>
2023-03-18 10:16:40 +0100

    Log delle mosse
    Aggiunta classe che funge da contenitore da log delle mosse.
    Questo log verrà utilizzato come base per capire le pedine catturate (mangiate).
    
    Issue: #15

---
+++ src/main/java/core/board/MoveLog.java
@@ -0,0 +1,34 @@
+package core.board;
+
+import core.movements.Move;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoveLog {
+    private final List<Move> moves;
+
+    public MoveLog() {
+        this.moves = new ArrayList<>();
+    }
+
+    public void addMove(final Move move) {
+        this.moves.add(move);
+    }
+
+    public Move removeMove(final int index) {
+        return this.moves.remove(index);
+    }
+
+    public boolean removeMove(final Move move) {
+        return this.moves.remove(move);
+    }
+
+    public int size() {
+        return this.moves.size();
+    }
+
+    public void clear() {
+        this.moves.clear();
+    }
+}
