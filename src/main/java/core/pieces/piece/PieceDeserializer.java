a5024e9fa2dc930561c556924667b2dcb15eb6c6
Angelo Rigoni <angelo.rigoni05@gmail.coom>
2023-03-20 08:38:43 +0100

    Serializzazione - refactoring
    Rimosso la serializzazione non necessaria. Mantenuto solo la deserializzazione per leggere lo stato iniziale delle pedine.

--- src/main/java/core/pieces/piece/PieceDeserializer.java
+++ src/main/java/core/pieces/piece/PieceDeserializer.java
@@ -4,16 +4,7 @@ import com.google.gson.*;
 
 import java.lang.reflect.Type;
 
-public class PieceDeserializer implements JsonSerializer<Piece>, JsonDeserializer<Piece> {
-
-    @Override
-    public JsonElement serialize(Piece src, Type typeOfSrc, JsonSerializationContext context) {
-        JsonObject result = new JsonObject();
-        result.add("type", new JsonPrimitive(src.getClass().getCanonicalName()));
-        result.add("properties", context.serialize(src, src.getClass()));
-
-        return result;
-    }
+public class PieceDeserializer implements JsonDeserializer<Piece> {
 
     @Override
     public Piece deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
